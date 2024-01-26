dd5584d8efa861f383c4656939006c7aa3736287
Antonio Wills <antontiowillissilvasantos@gmail.com>
2024-01-26 11:15:41 -0300

    tudo arrumado

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
 int main() {
+  /**
+   * Alteramdo arquivo já comitado para depois comitar 1.
+  */
+
+ /**
+   * Alteramdo arquivo já comitado para depois comitar 2.
+  */
   return 0;
 }
---
+++ teste1.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void testando(){
+    std::cout << "ola pessoasss";
+}
+
+void testando2() {
+   /**
+    * Alteramdo arquivo já comitado para depois comitar 3.
+   */
+}
